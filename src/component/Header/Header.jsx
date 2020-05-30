f05d462b5d2906f4f0de315144659fa0653b8402
elevli.aykut <elevli.aykut.28@gmail.com>
2020-05-31 02:17:54 +0300

    header improvements

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import styles from './Header.module.css';
+import { Typography } from '@material-ui/core';
 
 class Header extends Component {
     render() {
@@ -10,14 +11,7 @@ class Header extends Component {
             <div className="Header">
                 <Grid container spacing={3} className={styles.header}>
                     <Grid item xs="4" md="4" className={styles.warning}>
-                        <h1>Stay At Home</h1>
-
-                    </Grid>
-                    <Grid item xs="4" md="4" className={styles.logo}>
-                        <h1>Covid19.</h1>
-                    </Grid>
-                    <Grid item xs="4" md="4" className={styles.county}>
-                        <h3>Country</h3>
+                        <h1>#Stay At Home</h1>
                     </Grid>
                 </Grid>
             </div>
