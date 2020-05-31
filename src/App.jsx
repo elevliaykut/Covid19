207050ac5960e8c1c313799ea1050cb69fabf685
elevli.aykut <elevli.aykut.28@gmail.com>
2020-05-31 03:09:18 +0300

    content added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import Content from './component/Content/Content';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Content/>
     </div>
   );
 }
