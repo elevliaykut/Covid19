3a635b38120098bbde133ca8afd063fda7e31703
elevli.aykut <elevli.aykut.28@gmail.com>
2020-05-31 03:41:53 +0300

    content structure build

--- src/component/Content/Content.jsx
+++ src/component/Content/Content.jsx
@@ -1,9 +1,83 @@
 import React from 'react';
+import { makeStyles, StylesProvider } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import styles from './Content.module.css';
 
 const Content = () => {
     return (
-        <div>
-            Content
+        <div className="content">
+            <Grid container item xs="12" md="12">
+                <Grid item xs="3" md="3" className={styles.confirmed}>
+                    <Card>
+                        <CardContent>
+                            <Typography gutterBottom variant="5" component="h2">
+                                Confirmed
+                            </Typography>
+                        </CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                        </Typography>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs="3" md="3" className={styles.recovered}>
+                    <Card>
+                        <CardContent>
+                            <Typography gutterBottom variant="5" component="h2">
+                                Recovered
+                            </Typography>
+                        </CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                        </Typography>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs="3" md="3" className={styles.death}>
+                    <Card>
+                        <CardContent>
+                            <Typography gutterBottom variant="5" component="h2">
+                                Deaths
+                            </Typography>
+                        </CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                        </Typography>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     );
 }
