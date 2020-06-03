45083b4ae829f4c42995ea62a8c879868f637b5a
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-03 18:29:00 +0300

    axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,25 @@ import Header from './component/Header/Header';
 import Cards from './component/Cards/Cards';
 import Content from './component/Content/Content';
 import Footer from './component/Footer/Footer';
-function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Cards/>
-        <Content/>
-        <Footer/>
-    </div>
-  );
+import { fetchData } from './api';
+
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Cards />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
