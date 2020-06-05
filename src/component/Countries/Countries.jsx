8e5fee4a39e8c0eb2f8d806cfb760b27a505bdbc
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-05 16:30:46 +0300

    countries snippet improvements

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -18,11 +18,8 @@ const Countries = () => {
     return (
         <div>
             <Grid container item xs={12} md={12} className={styles.content}>
-                <Grid item xs={8} md={8} className={styles.title}>
+                <Grid item xs={2} md={2} className={styles.title}>
                     <h3>ALSO SEE</h3>
-                </Grid>
-                <Grid item xs={4} md={8} className={styles.total}>
-                    <h3>{fetchedCountries.length}</h3>
                 </Grid>
                 <Grid item xs={12} md={12} className={styles.countries}>
                     {fetchedCountries.map((country, i) => <div key={i} value={country}>{country}</div>)}
