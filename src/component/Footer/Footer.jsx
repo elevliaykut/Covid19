bff3d95733b504d993feb6bfe62a6a17e9c8af7a
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-07 23:03:07 +0300

    footer improvemnts

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import styles from './Footer.module.css';
+import Github from '../../icons/github.svg';
 
 const Footer = () => {
     return (
         <div>
             <Grid container item xs={12} md={12} className={styles.footer}>
-                <Grid item={12} md={12} className={styles.title}>
+                <Grid item={6} md={6} className={styles.title}>
                     <h3>A project by Aykut ELEVLİ</h3>
+                </Grid>
+                <Grid item={2} md={2} className={styles.githubLogo}>
+                    <img src={Github} className={styles.github}/>
                 </Grid>
             </Grid>
         </div>
