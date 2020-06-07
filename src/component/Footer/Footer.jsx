a4efed3224bdcdee5a18aa154947bcff60ddb7e4
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-07 23:29:30 +0300

    footer improvements

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import styles from './Footer.module.css';
 import Github from '../../icons/github.svg';
+import Linkedin from '../../icons/linkedin.svg';
+import WebSite from '../../icons/website.svg';
 
 const Footer = () => {
     return (
         <div>
             <Grid container item xs={12} md={12} className={styles.footer}>
-                <Grid item={6} md={6} className={styles.title}>
+                <Grid item xs={6} md={6} className={styles.title}>
                     <h3>A project by Aykut ELEVLİ</h3>
                 </Grid>
-                <Grid item={2} md={2} className={styles.githubLogo}>
+                <Grid item xs={2} md={2} className={styles.githubLogo}>
                     <img src={Github} className={styles.github}/>
                     <h5>Github</h5>
+                </Grid>
+                <Grid item xs={2} md={2} className={styles.linkedinlogo}>
+                    <img src={Linkedin} className={styles.linkedin}/>
+                    <h5>Linkedin</h5>
                 </Grid>
             </Grid>
         </div>
