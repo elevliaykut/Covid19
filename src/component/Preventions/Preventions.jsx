0437f60371e2fde4c54eb7565e82ac82be50eb67
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-09 15:39:51 +0300

    preventions improvemnts

--- src/component/Preventions/Preventions.jsx
+++ src/component/Preventions/Preventions.jsx
@@ -4,15 +4,18 @@ import Grid from '@material-ui/core/Grid';
 import Hand from '../../icons/hand.svg';
 import Mask from '../../icons/mask.svg';
 import Disinfect from '../../icons/disinfection.svg';
+import { Row, Col } from 'reactstrap';
 
 const Preventions = () => {
     return (
-        <div>
-            <Grid container item xs={12} md={12} className={styles.content}>
-                <Grid item xs={12} md={12} className={styles.preventions}>
+        <Grid item xs={12} md={12} className={styles.content}>
+            <Row>
+                <Col className={styles.preventions}>
                     <h2>Preventions</h2>
-                </Grid>
-                <Grid item xs={4} md={4} className={styles.washhands}>
+                </Col>
+            </Row>
+            <Row>
+                <Col className={styles.washhands}>
                     <img src={Hand} className={styles.handicon} />
                     <h4>Wash Your Hands</h4>
                     <h5>Wash your hands regularly, especially: </h5>
@@ -21,23 +24,17 @@ const Preventions = () => {
                     <li>after touching money. </li>
                     <li>before, during and after caring for a sick or vulnerable person. </li>
                     <li>before and after eating. </li>
-                </Grid>
-                <Grid item xs={4} md={4} className={styles.usemask}>
+                </Col>
+                <Col className={styles.usemask}>
                     <img src={Mask} className={styles.maskicon} />
                     <h4>Use Mask</h4>
                     <h5>Use mask regularly, especially: </h5>
                     <p>Since the start of the coronavirus outbreak some places have fully embraced wearing face mask, and anyone
                     caught without one risk becoming a social pariah.
                     </p>
-                </Grid>
-                <Grid item xs={4} md={4} className={styles.disinfect}>
-                    <img src={Disinfect} className={styles.disinfecticon} />
-                    <h4>Disinfect</h4>
-                    <li>after someone with suspected coronavirus (COVID-19)</li>
-                    <li>wear disposable or washing-up gloves and aprons for cleaning.</li>
-                </Grid>
-            </Grid>
-        </div>
+                </Col>
+            </Row>
+        </Grid>
     );
 }
 
