b3ef54e1d998d603a2c05e243f7541c6576fc678
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-09 23:39:59 +0300

    footer refactored

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -9,40 +9,39 @@ import { Row, Col } from 'reactstrap';
 
 const Footer = () => {
     return (
-        <div>
-            <Grid item xs={12} md={12} className={styles.footer}>
-                <Row>
-                    <Col className={styles.title}>
+        <Grid item xs={12} md={12} className={styles.footer}>
+            <Row>
+                <div className={styles.title}>
+                    <Col>
                         <h3>A project by Aykut ELEVLİ</h3>
                     </Col>
-                    <Col className={styles.githubLogo}>
-                        <a href="https://github.com/elevliaykut">
-                            <img src={Github} className={styles.github} />
-                            <h4>Github</h4>
-                        </a>
-                    </Col>
-                    <Col className={styles.linkedinLogo}>
-                        <a href="https://www.linkedin.com/in/aykut-elevli/">
-                            <img src={Linkedin} className={styles.linkedin} />
-                            <h4>Linkedin</h4>
-                        </a>
-                    </Col>
-                    <Col className={styles.mediumLogo}>
-                        <a href="https://medium.com/@elevli.aykut.28">
-                            <img src={Medium} className={styles.medium} />
-                            <h4>Medium</h4>
-                        </a>
-                    </Col>
-                    <Col className={styles.websiteLogo}>
-                        <a href="https://aykutelevli.me/">
-                            <img src={WebSite} className={styles.website} />
-                            <h4>WebSite</h4>
-                        </a>
-                    </Col>
-                </Row>
-            </Grid>
-        </div>
-
+                </div>
+                <Col className={styles.githubLogo}>
+                    <a href="https://github.com/elevliaykut">
+                        <img src={Github} className={styles.github} />
+                        <h4>Github</h4>
+                    </a>
+                </Col>
+                <Col className={styles.linkedinLogo}>
+                    <a href="https://www.linkedin.com/in/aykut-elevli/">
+                        <img src={Linkedin} className={styles.linkedin} />
+                        <h4>Linkedin</h4>
+                    </a>
+                </Col>
+                <Col className={styles.mediumLogo}>
+                    <a href="https://medium.com/@elevli.aykut.28">
+                        <img src={Medium} className={styles.medium} />
+                        <h4>Medium</h4>
+                    </a>
+                </Col>
+                <Col className={styles.websiteLogo}>
+                    <a href="https://aykutelevli.me/">
+                        <img src={WebSite} className={styles.website} />
+                        <h4>WebSite</h4>
+                    </a>
+                </Col>
+            </Row>
+        </Grid>
     );
 }
 
