6576336b6f20c5ed6ddc9973258ad7edb29a55d6
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-09 17:10:53 +0300

    preventions refactored

--- src/component/Preventions/Preventions.jsx
+++ src/component/Preventions/Preventions.jsx
@@ -5,6 +5,8 @@ import Hand from '../../icons/hand.svg';
 import Mask from '../../icons/mask.svg';
 import Disinfect from '../../icons/disinfection.svg';
 import { Row, Col } from 'reactstrap';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const Preventions = () => {
     return (
@@ -15,30 +17,26 @@ const Preventions = () => {
                 </Col>
             </Row>
             <Row>
-                <Col className={styles.washhands}>
-                    <img src={Hand} className={styles.handicon} />
-                    <h4>Wash Your Hands</h4>
-                    <h5>Wash your hands regularly, especially: </h5>
-                    <li>after blowing your nose, coughing or sneezing. </li>
-                    <li>after touching surfaces outside of the home.</li>
-                    <li>after touching money. </li>
-                    <li>before, during and after caring for a sick or vulnerable person. </li>
-                    <li>before and after eating. </li>
-                </Col>
-                <Col className={styles.usemask}>
-                    <img src={Mask} className={styles.maskicon} />
-                    <h4>Use Mask</h4>
-                    <h5>Use mask regularly, especially: </h5>
-                    <p>Since the start of the coronavirus outbreak some places have fully embraced wearing face mask, and anyone
-                    caught without one risk becoming a social pariah.
-                    </p>
-                </Col>
-                <Col className={styles.disinfect}>
-                    <img src={Disinfect} className={styles.disinfecticon} />
-                    <h4>Disinfect</h4>
-                    <li>after someone with suspected coronavirus (COVID-19)</li>
-                    <li>wear disposable or washing-up gloves and aprons for cleaning.</li>
-                </Col>
+                <Card className={styles.washhandscard}>
+                    <CardContent>
+                        <Row>
+                            <Col>
+                                <img src={Hand} className={styles.handicon} />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className={styles.washhandscontent}>
+                                <h4>Wash Your Hands</h4>
+                                <h5>Wash your hands regularly, especially: </h5>
+                                <li>after blowing your nose, coughing or sneezing. </li>
+                                <li>after touching surfaces outside of the home.</li>
+                                <li>after touching money. </li>
+                                <li>before, during and after caring for a sick or vulnerable person. </li>
+                                <li>before and after eating. </li>
+                            </Col>
+                        </Row>
+                    </CardContent>
+                </Card>
             </Row>
         </Grid>
     );
