cbdbaf210965b0180dd9eb8ed39bb1bdc089e93b
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-09 18:22:52 +0300

    footer refactored

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -5,40 +5,52 @@ import Github from '../../icons/github.svg';
 import Linkedin from '../../icons/linkedin.svg';
 import WebSite from '../../icons/website.svg';
 import Medium from '../../icons/medium.svg';
+import { Row, Col } from 'reactstrap';
 
 const Footer = () => {
     return (
-        <div>
-            <Grid container item xs={12} md={12} className={styles.footer}>
-                <Grid item xs={4} md={4} className={styles.title}>
-                    <h3>A project by Aykut ELEVLİ</h3>
-                </Grid>
-                <Grid item xs={2} md={2} className={styles.githubLogo}>
-                    <a href="https://github.com/elevliaykut">
-                        <img src={Github} className={styles.github} />
-                        <h4>Github</h4>
-                    </a>
-                </Grid>
-                <Grid item xs={2} md={2} className={styles.linkedinLogo}>
-                    <a href="https://www.linkedin.com/in/aykut-elevli/">
-                        <img src={Linkedin} className={styles.linkedin} />
-                        <h4>Linkedin</h4>
-                    </a>
-                </Grid>
-                <Grid item xs={2} md={2} className={styles.mediumLogo}>
-                    <a href="https://medium.com/@elevli.aykut.28">
-                        <img src={Medium} className={styles.medium} />
-                        <h4>Medium</h4>
-                    </a>
-                </Grid>
-                <Grid item xs={2} md={2} className={styles.websiteLogo}>
-                    <a href ="https://aykutelevli.me/">
-                        <img src={WebSite} className={styles.website} />
-                        <h4>WebSite</h4>
-                    </a>
-                </Grid>
-            </Grid>
-        </div>
+        <Grid item xs={12} md={12} className={styles.footer}>
+            <Row>
+                <Col>
+                    <Grid className={styles.title}>
+                        <h3>A project by Aykut ELEVLİ</h3>
+                    </Grid>
+                </Col>
+                <Col>
+                    <Grid className={styles.githubLogo}>
+                        <a href="https://github.com/elevliaykut">
+                            <img src={Github} className={styles.github} />
+                            <h4>Github</h4>
+                        </a>
+                    </Grid>
+                </Col>
+                <Col>
+                    <Grid className={styles.linkedinLogo}>
+                        <a href="https://www.linkedin.com/in/aykut-elevli/">
+                            <img src={Linkedin} className={styles.linkedin} />
+                            <h4>Linkedin</h4>
+                        </a>
+                    </Grid>
+                </Col>
+                <Col>
+                    <Grid className={styles.mediumLogo}>
+                        <a href="https://medium.com/@elevli.aykut.28">
+                            <img src={Medium} className={styles.medium} />
+                            <h4>Medium</h4>
+                        </a>
+                    </Grid>
+                </Col>
+                <Col>
+                    <Grid className={styles.websiteLogo}>
+                        <a href="https://aykutelevli.me/">
+                            <img src={WebSite} className={styles.website} />
+                            <h4>WebSite</h4>
+                        </a>
+                    </Grid>
+                </Col>
+            </Row>
+        </Grid>
+
     );
 }
 
