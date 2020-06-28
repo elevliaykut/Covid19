18095db66afb8eae497babbea7899fc3419b3609
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 21:42:29 +0300

    search bar added to header

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -2,6 +2,7 @@ import React, { Components, Component } from 'react';
 import styles from './Countries.module.css';
 import { getCountries, countryDetails } from '../../api/TotalValues';
 import Time from '../../icons/clock.svg';
+import Search from '../../icons/search.svg';
 
 class Countries extends Component {
     constructor() {
@@ -9,10 +10,60 @@ class Countries extends Component {
         this.state = {
             countries: '',
             countryDetails: [],
+            search: '',
             loadded: false
         }
     }
 
+    renderCountry = country => {
+        const { search } = this.state;
+
+        if (search !== "" && country.countryRegion.toLowerCase().indexOf(search.toLowerCase()) === -1) {
+            return null
+        }
+        return <div className="col-6">
+            <div className={styles.cards}>
+                <div className={styles.cardsheader}>
+                    <h4>{country.countryRegion},{country.provinceState}</h4>
+                </div>
+                <div className={styles.cardsbody}>
+                    <div className="row">
+                        <div className={styles.confirmed}>
+                            <label className={styles.confirmedcircle}></label>
+                            <label className={styles.confirmedtitle}>Confirmed:</label>
+                            <label className={styles.confirmedvalue}>{country.confirmed}</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className={styles.recovered}>
+                            <label className={styles.recoveredcircle}></label>
+                            <label className={styles.recoveredtitle}>Recovered:</label>
+                            <label className={styles.recoveredvalue}>{country.recovered}</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className={styles.deaths}>
+                            <label className={styles.deathscircle}></label>
+                            <label className={styles.deathstitle}>Deaths:</label>
+                            <label className={styles.deathsvalue}>{country.deaths}</label>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.cardsfooter}>
+                    <div className="row">
+                        <div className={styles.date}>
+                            <img src={Time} className={styles.timeicon} />
+                            <label className={styles.datetitle}>Date:</label>
+                            <label className={styles.datevalue}>{new Date(country.lastUpdate).toDateString()}</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    }
     componentDidMount() {
         getCountries().then(res => {
             this.setState({
@@ -27,16 +78,27 @@ class Countries extends Component {
             })
         });
     }
+
+    onChange = event => {
+        this.setState({ search: event.target.value });
+    }
+
     render() {
         if (!this.state.loadded) {
             return <div>Loading</div>
         }
-        return (  
+        return (
             <>
                 <div className={styles.countries}>
                     <div className={styles.header}>
                         <div className={styles.title}>
-                            <h5>Covid19</h5>
+                            <a href="/">
+                                <h5>Covid19</h5>
+                            </a>
+                        </div>
+                        <div className={styles.searchbar}>
+                            <img src={Search} className={styles.searchicon} />
+                            <input className={styles.search} placeholder="Search" onChange={this.onChange} />
                         </div>
                         <div className={styles.totalcard}>
                             <label>{this.state.countryDetails.length} Regions</label>
@@ -45,47 +107,11 @@ class Countries extends Component {
                     <div className={styles.body}>
                         <div className="container">
                             <div className="row">
-                                {this.state.countryDetails.map(countryDetail => (
-                                    <div className="col-6">
-                                        <div className={styles.cards}>
-                                            <div className={styles.cardsheader}>
-                                                <h4>{countryDetail.countryRegion},{countryDetail.provinceState}</h4>
-                                            </div>
-                                            <div className={styles.cardsbody}>
-                                                <div className="row">
-                                                    <div className={styles.confirmed}>
-                                                        <label className={styles.confirmedcircle}></label>
-                                                        <label className={styles.confirmedtitle}>Confirmed:</label>
-                                                        <label className={styles.confirmedvalue}>{countryDetail.confirmed}</label>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className={styles.recovered}>
-                                                        <label className={styles.recoveredcircle}></label>
-                                                        <label className={styles.recoveredtitle}>Recovered:</label>
-                                                        <label className={styles.recoveredvalue}>{countryDetail.recovered}</label>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className={styles.deaths}>
-                                                        <label className={styles.deathscircle}></label>
-                                                        <label className={styles.deathstitle}>Deaths:</label>
-                                                        <label className={styles.deathsvalue}>{countryDetail.deaths}</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className={styles.cardsfooter}>
-                                                <div className="row">
-                                                    <div className={styles.date}>
-                                                        <img src={Time} className={styles.timeicon} />
-                                                        <label className={styles.datetitle}>Date</label>
-                                                        <label className={styles.datevalue}>{new Date(countryDetail.lastUpdate).toDateString()}</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
+                                {
+                                    this.state.countryDetails.map(country => {
+                                        return this.renderCountry(country)
+                                    })
+                                }
                             </div>
                         </div>
                     </div>
