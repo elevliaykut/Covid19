d3441f8e5519b0da2af2b87743769c9de02fd424
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 15:29:45 +0300

    country card improvements

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,23 +1,29 @@
 import React, { Components, Component } from 'react';
 import styles from './Countries.module.css';
 import { getCountries } from '../../api/TotalValues';
+import Time from '../../icons/clock.svg';
 
 class Countries extends Component {
     constructor() {
         super();
         this.state = {
-            countries: ''
+            countries: '',
+            loadded: false
         }
     }
 
     componentDidMount() {
         getCountries().then(res => {
             this.setState({
-                countries: res
+                countries: res,
+                loadded: true
             })
         })
     }
     render() {
+        if(!this.state.loadded) {
+            return <div>Loading</div>
+        }
         return (
             <>
                 <div className={styles.countries}>
@@ -36,14 +42,38 @@ class Countries extends Component {
                                         </div>
                                         <div className={styles.cardsbody}>
                                             <div className="row">
-                                                <label className={styles.confirmedcircle}></label>
-                                                <label className={styles.confirmedtitle}>Confirmed</label>
-                                                <label className={styles.confirmedvalue}>121212121</label>
+                                                <div className={styles.confirmed}>
+                                                    <label className={styles.confirmedcircle}></label>
+                                                    <label className={styles.confirmedtitle}>Confirmed:</label>
+                                                    <label className={styles.confirmedvalue}>121212121</label>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className={styles.recovered}>
+                                                    <label className={styles.recoveredcircle}></label>
+                                                    <label className={styles.recoveredtitle}>Recovered:</label>
+                                                    <label className={styles.recoveredvalue}>121212121</label>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className={styles.deaths}>
+                                                    <label className={styles.deathscircle}></label>
+                                                    <label className={styles.deathstitle}>Deaths:</label>
+                                                    <label className={styles.deathsvalue}>121212121</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className={styles.cardsfooter}>
+                                            <div className="row">
+                                                <div className={styles.date}>
+                                                    <img src={Time} className={styles.timeicon} />
+                                                    <label className={styles.datetitle}>Date</label>
+                                                    <label className={styles.datevalue}>12.12.2020</label>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
