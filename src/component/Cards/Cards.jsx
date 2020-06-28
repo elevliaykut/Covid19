89521e2fc69b72ff487b6bc1231bb2e16cdd31ad
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 19:56:44 +0300

    link added to cards

--- src/component/Cards/Cards.jsx
+++ src/component/Cards/Cards.jsx
@@ -44,90 +44,96 @@ class Cards extends Component {
                     <Col>
                         <Card className={styles.confirmedcart}>
                             <CardContent>
-                                <Grid container item xs="12" md="12" className={styles.cardheader}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Icon} className={styles.confirmedicon} />
+                                <a href="/countries">
+                                    <Grid container item xs="12" md="12" className={styles.cardheader}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Icon} className={styles.confirmedicon} />
+                                        </Grid>
+                                        <Grid item xs="10" md="10" className={styles.title}>
+                                            <h3>Total Confirmed</h3>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs="10" md="10" className={styles.title}>
-                                        <h3>Total Confirmed</h3>
-                                    </Grid>
+                                    <Grid container item xs="12" md="12" className={styles.cardbody}>
+                                        <Grid item xs="12" md="12" className={styles.value}>
+                                            <CountUp start={0} end={this.state.confirmed} duration={2.5} separator="," />
+                                        </Grid>
+                                        <Grid item xs="12" md="12" className={styles.label}>
+                                            People
                                 </Grid>
-                                <Grid container item xs="12" md="12" className={styles.cardbody}>
-                                    <Grid item xs="12" md="12" className={styles.value}>
-                                        <CountUp start={0} end={this.state.confirmed} duration={2.5} separator="," />
                                     </Grid>
-                                    <Grid item xs="12" md="12" className={styles.label}>
-                                        People
-                                </Grid>
-                                </Grid>
-                                <Grid container className={styles.cardfooter}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Time} className={styles.timeicon} />
+                                    <Grid container className={styles.cardfooter}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Time} className={styles.timeicon} />
+                                        </Grid>
+                                        <Grid item="7" md="7" className={styles.date}>
+                                            <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item="7" md="7" className={styles.date}>
-                                        <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
-                                    </Grid>
-                                </Grid>
+                                </a>
                             </CardContent>
                         </Card>
                     </Col>
                     <Col>
                         <Card className={styles.recovered}>
                             <CardContent>
-                                <Grid container item xs="12" md="12" className={styles.cardheader}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Icon} className={styles.recoveredicon} />
+                                <a href="/countries">
+                                    <Grid container item xs="12" md="12" className={styles.cardheader}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Icon} className={styles.recoveredicon} />
+                                        </Grid>
+                                        <Grid item xs="10" md="10" className={styles.title}>
+                                            <h3>Total Recovered</h3>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs="10" md="10" className={styles.title}>
-                                        <h3>Total Recovered</h3>
-                                    </Grid>
+                                    <Grid container item xs="12" md="12" className={styles.cardbody}>
+                                        <Grid item xs="12" md="12" className={styles.value}>
+                                            <CountUp start={0} end={this.state.recovered} duration={2.5} separator="," />
+                                        </Grid>
+                                        <Grid item xs="12" md="12" className={styles.label}>
+                                            People
                                 </Grid>
-                                <Grid container item xs="12" md="12" className={styles.cardbody}>
-                                    <Grid item xs="12" md="12" className={styles.value}>
-                                        <CountUp start={0} end={this.state.recovered} duration={2.5} separator="," />
                                     </Grid>
-                                    <Grid item xs="12" md="12" className={styles.label}>
-                                        People
-                                </Grid>
-                                </Grid>
-                                <Grid container item xs="12" md="12" className={styles.cardfooter}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Time} className={styles.timeicon} />
+                                    <Grid container item xs="12" md="12" className={styles.cardfooter}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Time} className={styles.timeicon} />
+                                        </Grid>
+                                        <Grid item="7" md="7" className={styles.date}>
+                                            <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item="7" md="7" className={styles.date}>
-                                        <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
-                                    </Grid>
-                                </Grid>
+                                </a>
                             </CardContent>
                         </Card>
                     </Col>
                     <Col>
                         <Card className={styles.death}>
                             <CardContent>
-                                <Grid container item xs="12" md="12" className={styles.cardheader}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Icon} className={styles.deathicon} />
+                                <a href="/countries">
+                                    <Grid container item xs="12" md="12" className={styles.cardheader}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Icon} className={styles.deathicon} />
+                                        </Grid>
+                                        <Grid item xs="10" md="10" className={styles.title}>
+                                            <h3>Total Death</h3>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs="10" md="10" className={styles.title}>
-                                        <h3>Total Death</h3>
-                                    </Grid>
+                                    <Grid container item xs="12" md="12" className={styles.cardbody}>
+                                        <Grid item xs="12" md="12" className={styles.value}>
+                                            <CountUp start={0} end={this.state.deaths} duration={2.5} separator="," />
+                                        </Grid>
+                                        <Grid item xs="12" md="12" className={styles.label}>
+                                            People
                                 </Grid>
-                                <Grid container item xs="12" md="12" className={styles.cardbody}>
-                                    <Grid item xs="12" md="12" className={styles.value}>
-                                        <CountUp start={0} end={this.state.deaths} duration={2.5} separator="," />
                                     </Grid>
-                                    <Grid item xs="12" md="12" className={styles.label}>
-                                        People
-                                </Grid>
-                                </Grid>
-                                <Grid container item xs="12" md="12" className={styles.cardfooter}>
-                                    <Grid item xs="2" md="2">
-                                        <img src={Time} className={styles.timeicon} />
+                                    <Grid container item xs="12" md="12" className={styles.cardfooter}>
+                                        <Grid item xs="2" md="2">
+                                            <img src={Time} className={styles.timeicon} />
+                                        </Grid>
+                                        <Grid item="7" md="7" className={styles.date}>
+                                            <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item="7" md="7" className={styles.date}>
-                                        <h6>{new Date(this.state.lastUpdate).toDateString()}</h6>
-                                    </Grid>
-                                </Grid>
+                                </a>
                             </CardContent>
                         </Card>
                     </Col>
