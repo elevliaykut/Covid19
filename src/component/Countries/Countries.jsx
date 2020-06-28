a29afefdd839bab10a91c5ed8119fa594fa79323
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 15:03:48 +0300

    country cards improvements

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -19,11 +19,36 @@ class Countries extends Component {
     }
     render() {
         return (
-            <div className={styles.header}>
-                <div className={styles.totalcard}>
-                    <label>{this.state.countries.length} Countries</label>
+            <>
+                <div className={styles.countries}>
+                    <div className={styles.header}>
+                        <div className={styles.totalcard}>
+                            <label>{this.state.countries.length} Countries</label>
+                        </div>
+                    </div>
+                    <div className={styles.body}>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className={styles.cards}>
+                                        <div className={styles.cardsheader}>
+                                            <h4>Turkey</h4>
+                                        </div>
+                                        <div className={styles.cardsbody}>
+                                            <div className="row">
+                                                <label className={styles.confirmedcircle}></label>
+                                                <label className={styles.confirmedtitle}>Confirmed</label>
+                                                <label className={styles.confirmedvalue}>121212121</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
