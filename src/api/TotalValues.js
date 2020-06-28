81561202e72f2d22af8b085cca3a2bf0d1dc3108
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 12:56:48 +0300

    header improvements

--- src/api/TotalValues.js
+++ src/api/TotalValues.js
@@ -13,3 +13,17 @@ export const totalValues = () => {
             console.log(error);
         })
 }
+
+export const getCountries = () => {
+    return axios
+        .get('https://covid19.mathdro.id/api/countries', {
+            hedaer: { 'Content-Type': 'application/json' }
+        })
+        .then(response => {
+            console.log(response.data.countries);
+            return response.data.countries;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
 import styles from './Header.module.css';
+import { getCountries } from '../../api/TotalValues';
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            countries: ''
+        }
+    }
+
+    componentDidMount() {
+        getCountries().then(res => {
+            this.setState({
+                countries: res
+            })
+        })
+    }
     render() {
         return (
             <div className="Header">
                 <Grid container item xs="12" md="12" className={styles.header}>
-                    <Grid item xs="12" md="12" className={styles.title}>
+                    <Grid item xs="6" md="6" className={styles.title}>
                         <h3>Get to know about COVID-19.</h3>
+                    </Grid>
+                    <Grid item xs="6" md="6" className={styles.countries}>
+                        <a href="#">
+                            <label className={styles.country}>{this.state.countries.length} Country</label>
+                        </a>
                     </Grid>
                 </Grid>
             </div>
