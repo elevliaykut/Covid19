0762a31e2f0c513da85a062f9c6c7d31cc13a02c
elevli.aykut <elevli.aykut.28@gmail.com>
2020-06-28 13:36:15 +0300

    countries page improvements

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,10 +1,29 @@
-import React, {Components, Component} from 'react';
+import React, { Components, Component } from 'react';
 import styles from './Countries.module.css';
+import { getCountries } from '../../api/TotalValues';
 
 class Countries extends Component {
-    render(){
-        return(
-            <h2>as</h2>
+    constructor() {
+        super();
+        this.state = {
+            countries: ''
+        }
+    }
+
+    componentDidMount() {
+        getCountries().then(res => {
+            this.setState({
+                countries: res
+            })
+        })
+    }
+    render() {
+        return (
+            <div className={styles.header}>
+                <div className={styles.totalcard}>
+                    <label>{this.state.countries.length} Countries</label>
+                </div>
+            </div>
         )
     }
 }
